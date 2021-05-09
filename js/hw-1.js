ca2d6c32dbfc74f6364cb53da129635ca455409c
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-05-09 22:11:49 +0300

    Solved task 2
    Overriding the value of a variable

--- js/hw-1.js
+++
@@ -1,7 +0,0 @@
-// Change code below this line
-const productName = "Droid";
-console.log(productName);
-'Droid'
-const pricePerItem = 2000;
-console.log(pricePerItem);
-2000
---
+++ js/hw.js
@@ -0,0 +1,22 @@
+// Change code below this line
+// const productName = "Droid";
+// console.log(productName);
+// 'Droid'
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+// 2000
+
+// TASK 2
+
+//let productName = "Droid";
+//let pricePerItem = 2000;
+
+// Change code below this line
+
+let productName = "Droid";
+  productName = "Repair droid"; 
+console.log(productName);
+
+let pricePerItem = 2000;
+  pricePerItem = 3500;
+console.log(pricePerItem);
