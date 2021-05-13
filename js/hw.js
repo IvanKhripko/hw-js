2ac4aa2a8a1a5d40056f041aaf56aca4d301c795
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-05-13 12:55:26 +0300

    solved tasks 3, 4, 5, 6, 7
    NUMBERS, STRINGS, BOULES
    MATH OPERATORS
    TAMPLATE STRINGS
    PRODUCT ORDER
    FUNCTION DECLARATION AND CALL

--- js/hw.js
+++ js/hw.js
@@ -13,10 +13,62 @@
 
 // Change code below this line
 
-let productName = "Droid";
-  productName = "Repair droid"; 
-console.log(productName);
+// let productName = "Droid";
+// productName = "Repair droid";
+// console.log(productName);
 
-let pricePerItem = 2000;
-  pricePerItem = 3500;
-console.log(pricePerItem);
+// let pricePerItem = 2000;
+// pricePerItem = 3500;
+// console.log(pricePerItem);
+
+// TASK 3 (numbers, strings, boules)
+// Change code below this line
+
+// const topSpeed = 160;
+// let distance = 617.54;
+// const login = "mango395";
+// const isOnline = true;
+// const isAdmin = false;
+
+
+//TASK 4 (MATH OPERATORS)
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// // Change code below this line
+// const totalPrice = (pricePerItem * orderedQuantity);
+// console.log(totalPrice);
+
+
+// TASK 5 ( TAMPLATE STRINGS)
+
+// Change code below this line
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+
+// TASK 6 (PRODUCT ORDER)
+
+// const orderedQuantity = 6;
+// const pricePerDroid = 800;
+// const deliveryFee = 50;
+// const totalPrice = (orderedQuantity * pricePerDroid + deliveryFee);
+// const message = `You ordered droids worth ${totalPrice}. Delivery (${deliveryFee}
+// credits) is included in total price.`;
+// console.log(totalPrice);
+// console.log(message)
+
+// TASK 7 (FUNCTION DECLARATION AND CALL)
+
+// Задание
+// Объяви функцию sayHi, внутри которой добавь console.log() со 
+// строкой "Hello, this is my first function!".После объявления
+// вызови функцию sayHi.
+
+function sayHi() {
+  console.log("Hello, this is my first function!")
+}
+
+sayHi();
