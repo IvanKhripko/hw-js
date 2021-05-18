63f2f6d66fc9fb7247747246d5bf794ae944d739
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-05-18 09:47:16 +0300

    solved task 8
    PARAMETERS AND RESULTS

--- js/hw.js
+++ js/hw.js
@@ -60,6 +60,7 @@
 // console.log(totalPrice);
 // console.log(message)
 
+
 // TASK 7 (FUNCTION DECLARATION AND CALL)
 
 // Задание
@@ -67,8 +68,27 @@
 // строкой "Hello, this is my first function!".После объявления
 // вызови функцию sayHi.
 
-function sayHi() {
-  console.log("Hello, this is my first function!")
+// function sayHi() {
+//   console.log("Hello, this is my first function!")
+// }
+
+// sayHi();
+
+
+// TASK 8 (PARAMETERS AND RESULTS)
+
+// Функция add должна уметь складывать три числа и выводить результат в консоль.Добавь функции add три параметра
+// a, b и c, которые будут получать значения аргументов при её вызове.
+
+// Дополни console.log() так, чтобы он логировал строку "Addition result equals <result>", где < result > это сумма
+// переданных чисел.
+
+// Change code below this line
+function add(a,b,c) {
+  console.log(`Addition result equals ${a+b+c}`);
+  // Change code above this line
 }
 
-sayHi();
+add(15, 27, 10); 
+add(10, 20, 30);
+add(5, 10, 15);
