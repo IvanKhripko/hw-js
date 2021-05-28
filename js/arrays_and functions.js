6bf18a9938e1d049bfb77a4c9d6f149f185f8036
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-05-28 11:32:58 +0300

    solved task 11
    TASK 11 (jewelry engraving)

--- js/arrays_and functions.js
+++ js/arrays_and functions.js
@@ -158,3 +158,29 @@
 // console.log(splitMessage("Mango hurries to the train", " "));
 // console.log(splitMessage("Mango", ""))
 // console.log(splitMessage("best_for_week", "_"))
+
+
+// TASK 11 (jewelry engraving)
+// Сервису гравировки украшений нужна функция, которая бы автоматически считала цену
+// гравировки, в зависимости от количества слов и цены за слово.
+// Объявлена функция calculateEngravingPrice(message, pricePerWord).Эта функция принимает
+// строку, состоящую из слов разделённых только пробелами(параметр message) и цену
+// гравировки одного слова(параметр pricePerWord).
+// Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в
+// строке.
+
+function calculateEngravingPrice(message, pricePerWord) {
+   // Change code below this line
+    let price;
+    price = message.split(' ').length * pricePerWord;
+    // price = message.length * pricePerWord;
+    
+
+   // Change code above this line
+    return price;
+}
+
+console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+console.log(calculateEngravingPrice("JavaScript is in my blood", 20));
+console.log(calculateEngravingPrice("Web-development is creative work", 40));
+console.log(calculateEngravingPrice("Web-development is creative work", 20));
