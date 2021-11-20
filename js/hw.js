798c4b7316cc2dcfd9d7c9c86383921fbd5d1168
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-11-20 14:44:47 +0200

    Проверка плагина
    code run

--- js/hw.js
+++ js/hw.js
@@ -995,13 +995,15 @@
 // deliveryFee - стоимость доставки
 // Дополни код функции так, чтобы она возвращала сообщение о заказе в формате 
 // "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price.". Не забудь о цене доставки при вычислениях общей стоимости.
-function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
-  const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
-  let message = `You ordered droids worth ${'total price', totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//   let message = `You ordered droids worth ${'total price', totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
   
-  return message;
-}
+//   return message;
+// }
 
-console.log(makeOrderMessage(2, 100, 50));
-console.log(makeOrderMessage(4, 300, 100));
-console.log(makeOrderMessage(10, 70, 200));
+// console.log(makeOrderMessage(2, 100, 50));
+// console.log(makeOrderMessage(4, 300, 100));
+// console.log(makeOrderMessage(10, 70, 200));
+
+// console.log(makeOrderMessage(5, 200, 150));
