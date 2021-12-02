cf866637e0f19e18a827d2e19e808388e0a5a35a
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-12-02 23:44:36 +0200

    Решал задачи автопроверки
    Массивы и функции

--- js/arrays_and functions.js
+++ js/arrays_and functions.js
@@ -829,16 +829,227 @@
 
 
 
+// Дополни код так, чтобы переменные содержали частичные копии исходного массива
+// fruits.
+// firstTwoEls - массив из первых двух элементов
+// nonExtremeEls - массив из всех элементов кроме первого и последнего
+// lastThreeEls - массив из трёх последних элементов
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// const firstTwoEls = fruits.slice(0, 2);
+// console.log(firstTwoEls);
+// const nonExtremeEls = fruits.slice(1, -1);
+// console.log(nonExtremeEls);
+// const lastThreeEls = fruits.slice(2);
+// console.log(lastThreeEls);
 
 
+// Дополни код так, чтобы в переменной allClients получился массив всех элементов
+//  массивов oldClients и newClients.
+
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// const allClients = oldClients.concat(newClients); // Change this line
+// console.log(allClients);
 
 
+// Напиши функцию makeArray(firstArray, secondArray, maxLength) для создания 
+// нового массива со всеми элементами двух исходных firstArray и secondArray.
+// Параметр maxLength содержит максимально допустимую длину нового массива.
+// Если количество элементов нового массива больше maxLength, функция должна 
+// вернуть копию массива длиной maxLength элементов.В противном случае функция 
+// должна вернуть новый массив целиком.
+
+// function makeArray(firstArray, secondArray, maxLength) {
+// // создать новій массив из первіх двух параметров с помощью конкат
+//     const commonArray = firstArray.concat(secondArray);
+//     // return commonArray;
+//     if (commonArray.length >= maxLength) {
+//         const validArray = commonArray.slice(0, maxLength);
+//         return validArray;
+//     }
+    
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
 
 
+// Дополни цикл for так, чтобы он логировал все целые числа в диапазоне от start 
+// до end включительно.
+
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) { // Change this line
+//   console.log(i);
+// }
 
 
+// Напиши функцию calculateTotal(number), которая принимает целое число
+// (параметр number) и возвращает сумму всех целых чисел от единицы и до этого
+// числа.Например, если number равно 3, то сумма это 1 + 2 + 3, то есть 6.
+
+// function calculateTotal(number) {
+//     let total = 0;
+
+//     for (let i = 0; i <= number; i += 1) {
+//         total += i;
+//     }
+//     return total;
+// }
+
+// console.log(calculateTotal(1));
+// console.log(calculateTotal(3));
+// console.log(calculateTotal(7));
+// console.log(calculateTotal(18));
+// console.log(calculateTotal(24));
 
 
+// Дополни код цикла for так, чтобы он последовательно логировал все элементы 
+// массива fruits.
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i +=1) { // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
 
 
+// Напиши функцию calculateTotalPrice(order), которая принимает один параметр
+// order - массив чисел, и рассчитывает общую сумму его элементов.Общая сумма 
+// элементов должна сохраняться в переменной total, которая возвращается, как 
+// результат работы функции.
 
+// function calculateTotalPrice(order) {
+//     let total = 0;
+
+//     for (let i = 0; i < order.length; i += 1) {
+//         total += order[i];
+//     }
+//     return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+
+// Напиши функцию findLongestWord(string) которая принимает произвольную 
+// строку состоящую только из слов разделённых пробелом(параметр string) и 
+// возвращает самое длинное слово в этой строке.
+
+// function findLongestWord(string) {
+
+//     const convertedString = string.split(" ");
+//     let wordLength = convertedString[1].length;
+//     let longestWord;
+    
+//     for (let i = 0; i < convertedString.length; i += 1) {
+//         if (convertedString[i].length > wordLength) {
+//             longestWord = convertedString[i];
+//             return longestWord;
+//         }       
+//     }
+// }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// console.log(findLongestWord("Google do a roll"));
+// console.log(findLongestWord("May the force be with you"));
+
+
+// Дополни код функции createArrayOfNumbers(min, max) так, чтобы она возвращала 
+// массив всех целых чисел от значения min до max.
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+  
+//     for (let i = min; i <= max; i += 1) {
+//         numbers.push(i);
+//     }
+//   return numbers;
+// }
+
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+// console.log(createArrayOfNumbers(29, 34));
+
+
+// Напиши функцию filterArray(numbers, value), которая принимает массив чисел
+// (параметр numbers) и возвращает новый массив, в котором будут только те 
+// элементы массива numbers, которые больше чем значение параметра value(число).
+
+// function filterArray(numbers, value) {
+// // создать переменную с новым массивом
+//     const modifiedArray = [];
+// // перебрать массив numbers
+//     for (i = 0; i <= numbers.length; i += 1) {
+//         if (numbers[i] > value) {
+//             modifiedArray.push(numbers[i]);
+//         }
+//     }
+//     return modifiedArray;
+// // если в массиве numbers есть числа больше value запушить их в новый массив
+// // вернуть новый массив с новыми числами
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+// Функция checkFruit(fruit) принимает строку с названием фрукта(параметр fruit),
+// и проверяет есть ли такой фрукт в массиве fruits.
+// Дополни код функции так, что если:
+// фрукт есть в массиве, то функция возвращает true;
+// фрукта нет в массиве, то функция возвращает false.
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+//   return fruits.includes(fruit); // Change this line
+// }
+
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+
+// Общими элементами массивов называют те элементы, которые присутствуют во всех
+// массивах.
+// Например, в двух массивах[1, 3, 5] и[0, 8, 5, 3] общими будут числа 3 и 5,
+// т.к.они присутствуют в обоих исходных массивах.А числа 0, 1 и 8 присутствуют 
+// только в одном из массивов.
+// Напиши функцию getCommonElements(array1, array2) которая получает два массива
+//  произвольной длины в параметры array1 и array2, и возвращает новый массив,
+// состоящий из тех элементов, которые присутствуют в обоих исходных массивах.
+
+// function getCommonElements(array1, array2) {
+//     const arrayOfMatches = [];
+
+//     for (let i = 0; i < array1.length; i += 1) {
+//         const comparedItens = array2.includes(array1[i]);
+        
+//         if (comparedItens) {
+//             arrayOfMatches.push(array1[i]);
+//         }
+//     }
+//     return arrayOfMatches;
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
