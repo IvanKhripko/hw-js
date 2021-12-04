cdcb5a2f39b0f8d8dbe01a68d650a9a76e1210b3
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-12-04 22:40:19 +0200

    Зкончил решение задач автопроверки
    Массивы и функции

--- js/arrays_and functions.js
+++ js/arrays_and functions.js
@@ -1206,21 +1206,25 @@
 // При выполнении этой задачи в теле функции includes() нельзя использовать метод
 // массив.includes(значение).
 
-function includes(array, value) {
-    //  iterate over the array
-    for (i = 0; i < array.length; i += 1) {
-        if (array[i] === value) {
-            return true;
-        }
-    }
-    return false;
-}
+// function includes(array, value) {
+//     //  iterate over the array
+//     for (i = 0; i < array.length; i += 1) {
+//         if (array[i] === value) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 
 
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 17));
-console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
-console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
-console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
-console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(includes([1, 2, 3, 4, 5], 17));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+
+
+
+// РЕПЕТА УРОК
