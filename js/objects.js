159a7064ee1316ebfc31be43eed5fe4df5775fa1
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-12-06 22:59:15 +0200

    Решал задачи автопроверки
    Объекты

---
+++ js/objects.js
@@ -0,0 +1,164 @@
+// Присвой переменной apartment объект описывающий квартиру со следующими характеристиками:
+
+// imgUrl - фотография, значение "https://via.placeholder.com/640x480";
+// descr - описание, значение "Spacious apartment in the city center";
+// rating - рейтинг, значение 4;
+// price - цена, значение 2153;
+// tags - метаинформация, массив ["premium", "promoted", "top"].
+
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+// };
+
+
+// Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
+// name - имя владельца, значение "Henry";
+// phone - телефон, значение "982-126-1588";
+// email - почта, значение "henry.carter@aptmail.com".
+
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//     owner: {
+//         name: "Henry",
+//         phone: "982-126-1588",
+//         email: "henry.carter@aptmail.com",
+//     },
+// };
+
+
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// aptRating - рейтинг;
+// aptDescr - описание;
+// aptPrice - цена;
+// aptTags - теги.
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// const aptRating = apartment.rating;
+// console.log(aptRating);
+// const aptDescr = apartment.descr;
+// console.log(aptDescr);
+// const aptPrice = apartment.price;
+// console.log(aptPrice);
+// const aptTags = apartment.tags;
+// console.log(aptTags);
+
+
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// ownerName - имя владельца;
+// ownerPhone - телефон владельца;
+// ownerEmail - почта владельца;
+// numberOfTags - количество элементов массива в свойстве tags;
+// firstTag - первый элемент массива в свойстве tags;
+// lastTag - последний элемент массива в свойстве tags.
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// const ownerName = apartment.owner.name;
+// console.log(ownerName);
+// const ownerPhone = apartment.owner.phone;
+// console.log(ownerPhone);
+// const ownerEmail = apartment.owner.email;
+// console.log(ownerEmail);
+// const numberOfTags = apartment.tags.length;
+// console.log(numberOfTags);
+// const firstTag = apartment.tags[0];
+// console.log(firstTag);
+// const lastTag = apartment.tags[numberOfTags -1];
+// console.log(lastTag);
+
+
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment используя синтаксис «квадратных скобок».
+
+// aptRating - рейтинг;
+// aptDescr - описание;
+// aptPrice - цена;
+// aptTags - теги.
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// const aptRating = apartment['rating'];
+// console.log(aptRating);
+// const aptDescr = apartment['descr'];
+// console.log(aptDescr);
+// const aptPrice = apartment['price'];
+// console.log(aptPrice);
+// const aptTags = apartment['tags'];
+// console.log(aptTags);
+
+
+// Дополни код обновив значения свойств объекта apartment:
+
+// цену в свойстве price на 5000;
+// рейтинг квартиры в свойстве rating на 4.7;
+// имя владельца во вложенном свойстве name на "Henry Sibola";
+// массив тегов в свойстве tags добавив в конец строку "trusted".
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// apartment.price = 5000;
+// console.log(apartment.price);
+// apartment.rating = 4.7;
+// console.log(apartment.rating);
+// apartment.owner.name = "Henry Sibola";
+// console.log(apartment.owner.name);
+// apartment.tags.push("trusted");
+// console.log(apartment.tags);
+
+//  Object.keys(), Object.values(), Object.entries()
+// const hotel = {
+//   name: 'Resort Hotel',
+//   stars: 5,
+//   capacity: 100,
+// };
+
+// const keys = Object.keys(hotel); // ["name", "stars", "capacity"]
+// console.log(keys);
+// const values = Object.values(hotel); // ["Resort Hotel", 5, 100]
+// console.log(values);
+// const entries = Object.entries(hotel); // [Array(2), Array(2), Array(2)]
+// console.log(entries);
