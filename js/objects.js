85a26c8f91e835e510f5a09c38af087a8447e799
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-12-08 00:40:37 +0200

    Решал задачи автопроверки
    Объекты

--- js/objects.js
+++ js/objects.js
@@ -377,15 +377,15 @@
 //     { name: 'Droid', price: 400, quantity: 7 },
 //     { name: 'Grip', price: 1200, quantity: 9 },
 //   ];
-  
+ 
 // function getProductPrice(productName) {
 //     for (const product of products) {
 //         if (productName === product.name) {
-//             productName = product.price
+//             productName = product.price;
+//             return productName;
 //         }
-
 //     }
-//     return productName;
+//     return null;
 // }
 
 // console.log(getProductPrice("Radar"));
@@ -393,3 +393,320 @@
 // console.log(getProductPrice('Scanner'));
 // console.log(getProductPrice('Droid'));
 // console.log(getProductPrice('Engine'));
+
+
+// Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - имя (ключ) свойства. Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products. Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив.
+// const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+// function getAllPropValues(propName) {
+// const propertyValuesArray = [];
+
+//     for (const product of products) {
+//         const keys = Object.keys(product);
+
+//         for (const key of keys) {
+//             if (propName === key) {
+//                 propertyValuesArray.push(product[key]);
+//             }
+//         }     
+//     }
+//     return propertyValuesArray;
+// }
+
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
+
+
+// Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+// const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+  
+//   function calculateTotalPrice(productName) {
+//     let totalPrice = 0;
+
+//     for (const product of products) {
+//         const keys = Object.keys(product);
+//         for (const key of keys) {
+//             if (productName === product.name) {
+//                 totalPrice = product.quantity * product.price;
+//             }
+//         }
+//     }    
+//     return totalPrice;
+//   }
+
+//   console.log(calculateTotalPrice("Blaster"));
+//   console.log(calculateTotalPrice("Radar"));
+//   console.log(calculateTotalPrice("Droid"));
+//   console.log(calculateTotalPrice("Grip"));
+//   console.log(calculateTotalPrice("Scanner"));
+
+
+// Пришел трёхдневный прогноз максимальных температур и мы считаем среднюю температуру за три дня (meanTemperature). Замени объявления переменных yesterday, today и tomorrow одной операцией деструктуризации свойств объекта highTemperatures.
+
+// const highTemperatures = {
+//     yesterday: 28,
+//     today: 26,
+//     tomorrow: 33,
+//   };
+//   // Change code below this line
+//   const { yesterday, today, tomorrow } = highTemperatures;
+//   console.log(yesterday, today, tomorrow);
+//   // Change code above this line
+//   const meanTemperature = (yesterday + today + tomorrow) / 3;
+//   console.log(meanTemperature);
+
+
+// В прогнозе максимальных температур также может быть необязательное свойство icon - иконка погоды. Замени объявления переменных yesterday, today, tomorrow и icon одной операцией деструктуризации свойств объекта highTemperatures. Задай значение по умолчанию для icon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+// const highTemperatures = {
+//     yesterday: 28,
+//     today: 26,
+//     tomorrow: 33,
+//   };
+  
+//   const {
+//       yesterday,
+//       today,
+//       tomorrow,
+//       icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+//   } = highTemperatures;
+  
+//   const meanTemperature = (yesterday + today + tomorrow) / 3;
+//   console.log(meanTemperature);
+//   console.log(icon);
+
+
+// Замени объявления переменных highYesterday, highToday, highTomorrow и highIcon одной операцией деструктуризации свойств объекта highTemperatures.Задай значение по умолчанию для highIcon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+
+// const {
+//     yesterday: highYesterday,
+//     today: highToday,
+//     tomorrow: highTomorrow,
+//     icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// console.log(meanTemperature);
+// console.log(highIcon);
+
+
+// Выполни рефакторинг цикла for...of так, чтобы в нём использовалась деструктуризация объекта..
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+// Мы получили прогноз погоды на два дня, с минимальными и максимальными температурами, а также необязательными иконками. Замени объявления всех переменных одной операцией деструктуризации свойств объекта forecast. Задай значение по умолчанию для иконок, переменных todayIcon и tomorrowIcon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const {
+//     today: {
+//     high: highToday, 
+//     low: lowToday,
+//     icon: todayIcon ="https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//     },
+    
+//     tomorrow: {
+//         low: lowTomorrow,
+//         high: highTomorrow,
+//         icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//     },
+// } = forecast;
+
+
+// Функция calculateMeanTemperature(forecast) принимает один параметр forecast - объект температур на два дня следующего формата.
+
+// {
+//   today: { low: 10, high: 20 },
+//   tomorrow: { low: 20, high: 30 }
+// }
+// Замени объявления переменных todayLow, todayHigh, tomorrowLow и tomorrowHigh одной операцией деструктуризации свойств объекта forecast.
+
+// Change code below this line
+// function calculateMeanTemperature(forecast) {
+//     const { today: {
+//         low: todayLow,
+//         high: todayHigh,
+//     }, 
+//         tomorrow: {
+//             low: tomorrowLow,
+//             high: tomorrowHigh,
+//         }
+//     } = forecast;
+
+//     return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+// console.log(calculateMeanTemperature({
+//     today: {
+//         low: 28,
+//         high: 32,
+//     },
+//     tomorrow: {
+//         low: 25,
+//         high: 29,
+//     }
+// }));
+// console.log(calculateMeanTemperature({
+//     today: {
+//         low: 37,
+//         high: 40,
+//     },
+//     tomorrow: {
+//         low: 33,
+//         high: 38,
+//     }
+// }));
+
+
+// В переменной scores хранится массив результатов тестирования.Используя распыление и методы Math.max() и Math.min() дополни код так, чтобы в переменной bestScore был самый высокий балл, а в worstScore самый низкий.
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// console.log(bestScore);
+// const worstScore = Math.min(...scores);
+// console.log(worstScore);
+
+
+// В переменных firstGroupScores, secondGroupScores и thirdGroupScores хранятся результаты тестирования отдельных групп. Используя распыление дополни код так, чтобы:
+
+// В переменной allScores хранился массив всех результатов от первой до третьей группы.
+// В переменной bestScore был самый высокий общий балл.
+// В переменной worstScore был самый низкий общий балл.
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [
+//     ...firstGroupScores,
+//     ...secondGroupScores,
+//     ...thirdGroupScores,
+// ];
+// console.log(allScores);
+// const bestScore = Math.max(...allScores);
+// console.log(bestScore);
+// const worstScore = Math.min(...allScores);
+// console.log(worstScore);
+
+
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+
+// const finalSettings = {
+//     ...defaultSettings,
+//     ...overrideSettings,
+// };
+
+// console.log(finalSettings);
+
+
+// Напиши функцию makeTask(data) которая принимает один параметр data - объект со следующими свойствами.
+
+// text - текст задачи.
+// category - категория задачи.
+// priority - приоритет задачи.
+// Функция должна составить и вернуть новый объект задачи, не изменяя напрямую параметр data. В новом объекте должно быть свойство completed, значение которого хранится в одноимённой локальной переменной.
+
+// В параметре data гарантированно будет только свойство text, а остальные два, category и priority, могут отсутствовать. Тогда, в новом объекте задачи, в свойствах category и priority должны быть значения по умолчанию, хранящиеся в одноимённых локальных переменных.
+
+// function makeTask(data) {
+//     const defaultTaskObject = {
+//         category: 'General',
+//         priority: 'Normal',
+//         // completed: false,
+//     };
+
+//     const newObjectTask = {
+//         ...data,
+//     };
+
+//     const finalObjectTask = {
+//         ...defaultTaskObject,
+//         ...newObjectTask,
+//         completed: false,
+//     }
+
+//     return finalObjectTask;
+// }
+
+// console.log(makeTask({}));
+
+// console.log(makeTask({
+//     category: "Homemade",
+//     priority: "Low",
+//     text: "Take out the trash",
+// }));
+
+// console.log(makeTask({
+//     category: "Finance",
+//     text: "Take interest",
+// }));
+
+// console.log(makeTask({
+//     priority: "Low",
+//     text: "Choose shampoo",
+// }));
+
+// console.log(makeTask({
+//     text: "Buy bread" 
+// }));
