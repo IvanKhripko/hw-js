2701c4592794d0319cb2df007e0c88aa9fc5b35b
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2021-12-19 00:42:38 +0200

    Решал задачи автопроверки
    коллбэки и методы массивов

--- js/callback.js
+++ js/callback.js
@@ -1407,3 +1407,44 @@
 // };
 
 // console.log(isAnyUserActive(users));
+
+// ====================================================================
+
+// 34.Игровому сервису необходим функционал подсчёта среднего времени проведённого в играх. Дополни код так, чтобы в переменной totalPlayTime получилось общее игровое время из массива playtimes.
+
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// // Change code below this line
+
+// const totalPlayTime = playtimes.reduce((previousValue, time) => {
+//   return previousValue + time;
+// }, 0);
+
+// console.log(totalPlayTime);
+
+//=======================================================================
+
+// 35.Нашему сервису необходимо рассчитать среднее время проведённое в одной игре для каждого игрока, и получить общую сумму этих времён. Рассчитать время для каждого из игроков, можно разделив его время (свойство playtime) на количество игр (свойство gamesPlayed).
+
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce((total, player) => {
+//   const averageTime = player.playtime / player.gamesPlayed;
+//   return averageTime + total;
+  
+// }, 0);
+
+// console.log(totalAveragePlaytimePerGame);
+
+
